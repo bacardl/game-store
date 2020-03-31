141f6c9b5822a049b89f2e4aab90f3c89cc865df
Konstantin Yavorovenko <k.yavorovenko@gmail.com>
2020-03-31 16:08:02 +0300

    Implemented simple logic.

--- GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -4,6 +4,8 @@ using System.Configuration;
 using System.Web.Mvc;
 using Moq;
 using Ninject;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
 
 namespace GameStore.WebUI.Infrastructure
 {
@@ -29,7 +31,14 @@ namespace GameStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            // Здесь размещаются привязки
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game> 
+            {
+                new Game { Name = "SimCity", Price = 1499},
+                new Game { Name = "TITANFALL", Price = 2299},
+                new Game { Name = "Battlefield 4", Price = 899.4M}
+            });
+            kernel.Bind<IGameRepository>().ToConstant(mock.Object);
         }
     }
 }
