ed036a1e9abe64f09f02c1e19a930fd3e6f43cc6
Konstantin Yavorovenko <k.yavorovenko@gmail.com>
2020-04-01 07:11:54 +0300

    Implemented shopping cart feature + unit tests.

---
+++ GameStore.Domain/Entities/ShippingDetails.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Domain.Entities
+{
+    public class ShippingDetails
+    {
+        [Required(ErrorMessage = "Type your name!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Address fiels is required!")]
+        [Display(Name = "The first address")]
+        public string Line1 { get; set; }
+
+        [Display(Name = "The second address")]
+        public string Line2 { get; set; }
+
+        [Display(Name = "The third address")]
+        public string Line3 { get; set; }
+
+        [Required(ErrorMessage = "City is required")]
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+
+        public bool GiftWrap { get; set; }
+    }
+}
---
+++ GameStore.UnitTests/CartTests.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+using GameStore.Domain.Entities;
+using GameStore.Domain.Abstract;
+using Moq;
+using GameStore.WebUI.Controllers;
+using System.Web.Mvc;
+using GameStore.WebUI.Models;
+
+namespace GameStore.UnitTests
+{
+    [TestClass]
+    public class CartTests
+    {
+        [TestMethod]
+        public void Can_Add_New_Lines()
+        {
+            Game game1 = new Game { GameId = 1, Name = "Игра1" };
+            Game game2 = new Game { GameId = 2, Name = "Игра2" };
+
+            Cart cart = new Cart();
+
+            cart.AddItem(game1, 1);
+            cart.AddItem(game2, 1);
+            List<CartLine> results = cart.Lines.ToList();
+
+            Assert.AreEqual(results.Count(), 2);
+            Assert.AreEqual(results[0].Game, game1);
+            Assert.AreEqual(results[1].Game, game2);
+        }
+
+        [TestMethod]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            Game game1 = new Game { GameId = 1, Name = "Игра1" };
+            Game game2 = new Game { GameId = 2, Name = "Игра2" };
+
+            Cart cart = new Cart();
+
+            cart.AddItem(game1, 1);
+            cart.AddItem(game2, 1);
+            cart.AddItem(game1, 5);
+            List<CartLine> results = cart.Lines.OrderBy(c => c.Game.GameId).ToList();
+
+            Assert.AreEqual(results.Count(), 2);
+            Assert.AreEqual(results[0].Quantity, 6);
+            Assert.AreEqual(results[1].Quantity, 1);
+        }
+
+        [TestMethod]
+        public void Can_Remove_Line()
+        {
+            Game game1 = new Game { GameId = 1, Name = "Игра1" };
+            Game game2 = new Game { GameId = 2, Name = "Игра2" };
+            Game game3 = new Game { GameId = 3, Name = "Игра3" };
+
+            Cart cart = new Cart();
+
+            cart.AddItem(game1, 1);
+            cart.AddItem(game2, 4);
+            cart.AddItem(game3, 2);
+            cart.AddItem(game2, 1);
+
+            cart.RemoveLine(game2);
+
+            Assert.AreEqual(cart.Lines.Where(c => c.Game == game2).Count(), 0);
+            Assert.AreEqual(cart.Lines.Count(), 2);
+        }
+
+        [TestMethod]
+        public void Calculate_Cart_Total()
+        {
+            Game game1 = new Game { GameId = 1, Name = "Игра1", Price = 100 };
+            Game game2 = new Game { GameId = 2, Name = "Игра2", Price = 55 };
+
+            Cart cart = new Cart();
+
+            cart.AddItem(game1, 1);
+            cart.AddItem(game2, 1);
+            cart.AddItem(game1, 5);
+            decimal result = cart.ComputeTotalValue();
+
+            Assert.AreEqual(result, 655);
+        }
+
+        [TestMethod]
+        public void Can_Clear_Contents()
+        {
+            Game game1 = new Game { GameId = 1, Name = "Игра1", Price = 100 };
+            Game game2 = new Game { GameId = 2, Name = "Игра2", Price = 55 };
+
+            Cart cart = new Cart();
+
+            cart.AddItem(game1, 1);
+            cart.AddItem(game2, 1);
+            cart.AddItem(game1, 5);
+            cart.Clear();
+
+            Assert.AreEqual(cart.Lines.Count(), 0);
+        }
+
+        [TestMethod]
+        public void Can_Add_To_Cart()
+        {
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game> {
+                new Game {GameId = 1, Name = "Игра1", Category = "Кат1"},
+            }.AsQueryable());
+
+            Cart cart = new Cart();
+
+            CartController controller = new CartController(mock.Object);
+
+            controller.AddToCart(cart, 1, null);
+
+            Assert.AreEqual(cart.Lines.Count(), 1);
+            Assert.AreEqual(cart.Lines.ToList()[0].Game.GameId, 1);
+        }
+
+        [TestMethod]
+        public void Adding_Game_To_Cart_Goes_To_Cart_Screen()
+        {
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game> {
+                new Game {GameId = 1, Name = "Игра1", Category = "Кат1"},
+            }.AsQueryable());
+
+            Cart cart = new Cart();
+
+            CartController controller = new CartController(mock.Object);
+
+            RedirectToRouteResult result = controller.AddToCart(cart, 2, "myUrl");
+
+            Assert.AreEqual(result.RouteValues["action"], "Index");
+            Assert.AreEqual(result.RouteValues["returnUrl"], "myUrl");
+        }
+
+        [TestMethod]
+        public void Can_View_Cart_Contents()
+        {
+            Cart cart = new Cart();
+
+     
+            CartController target = new CartController(null);
+
+ 
+            CartIndexViewModel result
+                = (CartIndexViewModel)target.Index(cart, "myUrl").ViewData.Model;
+
+            Assert.AreSame(result.Cart, cart);
+            Assert.AreEqual(result.ReturnUrl, "myUrl");
+        }
+    }
+}
