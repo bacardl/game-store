80169b221a10c4f3b54166221d751f0d3f7004cd
Konstantin Yavorovenko <k.yavorovenko@gmail.com>
2020-04-01 04:53:30 +0300

    Added partial view, changed list view, added Bootstrap, changed routing config.

--- GameStore.UnitTests/UnitTest1.cs
+++ GameStore.UnitTests/UnitTest1.cs
@@ -31,9 +31,9 @@ namespace GameStore.UnitTests
             GameController controller = new GameController(mock.Object);
             controller.pageSize = 3;
             // Действие (act)
-            IEnumerable<Game> result = (IEnumerable<Game>)controller.List(2).Model;
+            GamesListViewModel result = (GamesListViewModel)controller.List(2).Model;
             // Утверждение (assert)
-            List<Game> games = result.ToList();
+            List<Game> games = result.Games.ToList();
             Assert.IsTrue(games.Count == 2);
             Assert.AreEqual(games[0].Name, "Игра4");
             Assert.AreEqual(games[1].Name, "Игра5");
@@ -64,6 +64,34 @@ namespace GameStore.UnitTests
                 + @"<a class=""btn btn-default"" href=""Page3"">3</a>",
                 result.ToString());
 
+        }
+
+        [TestMethod]
+        public void Can_Send_Pagination_View_Model()
+        {
+            // arrange
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game { GameId = 1, Name = "Игра1"},
+                new Game { GameId = 2, Name = "Игра2"},
+                new Game { GameId = 3, Name = "Игра3"},
+                new Game { GameId = 4, Name = "Игра4"},
+                new Game { GameId = 5, Name = "Игра5"}
+            });
+            GameController controller = new GameController(mock.Object);
+            controller.pageSize = 3;
+
+            // Act
+            GamesListViewModel result
+                = (GamesListViewModel)controller.List(2).Model;
+
+            // Assert
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems, 5);
+            Assert.AreEqual(pageInfo.TotalPages, 2);
         }
     }
 }
